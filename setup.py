58df98255fdb43392617518ecac3df7e0f955ee5
Dan Rose <dan@digilabs.io>
2019-04-25 19:57:34 -0500

    Setup script tweaks

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
-from glob import glob
-import os
+from pathlib import Path
 from xml.etree import ElementTree
 
 from setuptools import setup
 
-package = ElementTree.parse('package.xml')
+here = Path(__file__).parent
+package = ElementTree.parse(here / 'package.xml')
 name = package.findtext('name')
 
-properties_from_ros2_package = dict(
+colcon_properties = dict(
+    # should be distinct among PyPI packages so add a prefix
     name='ros2-{}'.format(name),
     version=package.findtext('version'),
     description=package.findtext('description'),
@@ -18,9 +19,12 @@ properties_from_ros2_package = dict(
     maintainer=package.find('maintainer').text,
     maintainer_email=package.find('maintainer').attrib['email'],
 )
-setup(**properties_from_ros2_package,
+
+launchfiles = [str(p.relative_to(here)) for p in (here.rglob('*.launch.py'))]
+assert launchfiles
+
+setup(**colcon_properties,
       data_files=[
-          (os.path.join('share', name), ['package.xml']),
-          (os.path.join('share', name, 'launch'), glob(os.path.join('launch', '*.launch.py'))),
-      ],
-      )
+          (str(Path('share', name)), ['package.xml']),
+          (str(Path('share', name, 'launch')), launchfiles),
+      ])
