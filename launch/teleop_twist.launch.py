2b478d8e70f0aaacb11bb4d97849b3d5a642c10b
Dan Rose <dan@digilabs.io>
2019-04-30 20:19:00 -0500

    Update linear scale in launch script too

--- launch/teleop_twist.launch.py
+++ launch/teleop_twist.launch.py
@@ -11,7 +11,7 @@ def generate_launch_description():
         parameters=[{'exit_on_esc': True}], arguments=['__log_level:=warn'])
     twist_node = Node(
         package='keystroke', node_executable='keystroke_arrows_to_twist', output='screen', node_name='arrows_to_twist',
-        parameters=[{'publish_period': 0.1, 'linear_scale': 1.0, 'angular_scale': 0.2}])
+        parameters=[{'publish_period': 0.1, 'linear_scale': 0.1, 'angular_scale': 0.2}])
     return LaunchDescription([
         keystroke_node,
         twist_node,
